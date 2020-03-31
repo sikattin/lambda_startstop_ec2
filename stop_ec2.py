779d96ed2bed419eef8c7bb635c04242e634fcd7
shikano.takeki <distortion0mix@gmail.com>
2020-03-31 15:37:25 +0900

    first commit

---
+++ stop_ec2.py
@@ -0,0 +1,26 @@
+import boto3
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+ 
+def lambda_handler(event, context):
+    logger.info("Event: " + str(event))
+    
+    region = 'ap-northeast-1'
+    instances = event['Instances']
+    try:
+        ec2 = boto3.client('ec2', region_name=region)
+        ec2.stop_instances(InstanceIds=instances)
+    except Exception as e:
+        logger.error(str(e))
+        return {
+            "statusCode": 500,
+            "message": 'An error has occured at automatically stopping ec2 instances'
+        }
+    else:
+        logger.info("Succeeded to stop instances... {0}".format(', '.join(instances)))
+        return {
+            "statusCode": 200,
+            "message": 'Finished automatically stopping ec2 instances'
+        }
